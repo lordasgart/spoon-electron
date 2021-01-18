3afa7a8367b889bd8d09630faacf828a71ba07ab
Markus Malz <markus.malz@it2media.de>
2021-01-18 06:07:10 +0100

    Moved

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow;
+"use strict";
+const { app, BrowserWindow } = require('electron');
 var os = require('os');
 console.log('hallo');
 var git = require('git-utils');
 var repository = git.open('C:\\Users\\lordasgart\\Projects\\spoon-electron');
 var head = repository.getHead();
 console.log(head);
-/*
-var repository = git.open('/home/lordasgart/Projects/spoon-electron')
-*/
 function createWindow() {
-    var win = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 720,
         height: 564,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
     });
     win.webContents.openDevTools();
     win.loadFile('index.html');
 }
 app.whenReady().then(createWindow);
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-app.on('activate', function () {
+app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
+//# sourceMappingURL=main.js.map
